d2643bd20506bcc927363f67ba08f2dbd25242b9
jspbtr <jspidersdrive@gmail.com>
2020-01-22 16:59:22 +0530

    1st commit

---
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/App.java
@@ -0,0 +1,15 @@
+package net.kzn.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       System.out.println("main starts");
+      
+       System.out.println("main ends");
+    }
+}
